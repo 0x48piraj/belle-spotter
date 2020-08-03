197e10919956a6dbab17f2eb83b11f7e5d8e1152
Piyush Raj <piyush.189303055@muj.manipal.edu>
2020-08-03 16:55:52 +0530

    major: camera ready

--- belle/driver.py
+++ belle/driver.py
@@ -1,11 +1,27 @@
 import face_recognition
+import os
 import cv2
 import math
 import numpy as np
+from dotenv import load_dotenv
 
-# performance tweaks
-#   1. process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. only detect faces in every other frame of video
+
+load_dotenv()
+
+# get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+if os.getenv("SAMY_MODE") == 'true':
+    target = face_recognition.load_image_file(os.getenv("SAMY_FILENAME"))
+    known_face_encoding = face_recognition.face_encodings(target)[0]
+
+# init variables
+score = 0
+face_locations = []
+face_encodings = []
+results = []
+process_this_frame = True
+
 
 def phi(landmarks):
     """
@@ -35,36 +51,25 @@ def phi(landmarks):
 
     # nasion-<a>-subnasale-<b>-mention (a = 3/4 x b) (a = 43%, b = 57%)
     NSM = subnasale_mention / (nasion_subnasale + subnasale_mention)
-    _bool_NSM = abs(NSM - 0.57) <= 0.01 # initial scalar (should be adjusted later)
+    bool_NSM = abs(NSM - 0.57) <= 0.01 # initial scalar (should be adjusted later)
 
     FWER = math.dist(ext_right_eye, int_right_eye) / width_face # eye - facial width ratio (right eye)
-    _bool_FWER = abs(FWER - 0.20) <= 0.01 # initial scalar (should be adjusted later)
+    bool_FWER = abs(FWER - 0.20) <= 0.01 # initial scalar (should be adjusted later)
 
     FWNR = math.dist(ext_left_nose, ext_right_nose) / width_face # intercanthal distance - facial width ratio
-    _bool_FWNR = abs(FWNR - 0.20) <= 0.01 # initial scalar (should be adjusted later)
+    bool_FWNR = abs(FWNR - 0.20) <= 0.01 # initial scalar (should be adjusted later)
+
+    score = (sum([bool_FWER, bool_FWNR, bool_NSM]) / 3) * 100
 
     _top_face = tuple(np.subtract(bottom_face, (0, phi))) # ideal top (not actual)
-    return _top_face, bottom_face, left_face, right_face, ext_right_eye, ext_left_eye, ext_right_nose, ext_left_nose, top_nose, bottom_nose
+    return score, _top_face, bottom_face, left_face, right_face, ext_right_eye, ext_left_eye, ext_right_nose, ext_left_nose, top_nose, bottom_nose
 
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("barak.webp")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-known_face_encoding = obama_face_encoding
-
-# Initialize some variables
-score = 0
-face_locations = []
-face_encodings = []
-results = []
-process_this_frame = True
-
+# performance tweaks
+#   1. process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. only detect faces in every other frame of video
 while True:
-    # Grab a single frame of video
+    # grab a single frame of video
     ret, frame = video_capture.read()
 
     # only process every other frame of video to save time
@@ -77,23 +82,29 @@ while True:
         
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        if len(face_locations) > 1 or len(face_locations) == 0:
-            print("Too many or too less people...")
-            continue
         face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        results = []
-        # TODO
-        # perform the golden ratio stuff on each of the faces in the frame
-        # append the results
-        _top_face, bottom_face, left_face, right_face, ext_right_eye, ext_left_eye, ext_right_nose, ext_left_nose, top_nose, bottom_nose = phi(face_landmarks_list)
-        match = face_recognition.compare_faces([known_face_encoding], face_encodings[0])
+        if len(face_locations) > 1 or len(face_locations) == 0:
+            start, end = (0,0), (int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+            cv2.rectangle(frame, start, end, (0, 0, 0), cv2.FILLED)
+            cv2.putText(frame, 'No person detected. System idle.', (0,130), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255)) if len(face_locations) == 0 else cv2.putText(frame, 'Only one person in the camera frame, please.', (0,130), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255))
+            cv2.imshow('Video', frame)
+            # hit 'q' on the keyboard to quit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            continue
 
-        if True in match:
-            score = 100
+        results = []
+        score, _top_face, bottom_face, left_face, right_face, ext_right_eye, ext_left_eye, ext_right_nose, ext_left_nose, top_nose, bottom_nose = phi(face_landmarks_list)
 
         result = "Your score: {}%".format(score)
+
+        if os.getenv("SAMY_MODE") == 'true':
+            match = face_recognition.compare_faces([known_face_encoding], face_encodings[0])
+            if True in match:
+                result = os.getenv("SAMY_MESSAGE")
+
         results.append(result)
 
     process_this_frame = not process_this_frame
